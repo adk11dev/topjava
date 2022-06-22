25318dfdad4817bc8529195c796f6ab7aa671277
Darkhan <adk11dev@gmail.com>
2022-06-22 16:33:08 +0600

    Implemented JdbcMealRepository. Supplemented sql scripts for meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -10,3 +11,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals(datetime, description, calories, user_id)
+VALUES ('2020-01-30 10:00:00.0', 'Завтрак', 500, 100000),
+       ('2020-01-30 13:00:00.0', 'Обед', 1000, 100000),
+       ('2020-01-30 20:00:00.0', 'Ужин', 500, 100000),
+       ('2020-01-30 23:59:00.0', 'Еда на граничное значение', 1, 100000),
+       ('2020-01-31 10:00:00.0', 'Завтрак', 1000, 100000),
+       ('2020-01-31 13:00:00.0', 'Обед', 500, 100000),
+       ('2020-01-31 20:00:00.0', 'Ужин', 410, 100000);
